7ecae2a097b3fa5e93399e849600e8e0d3117da8
Alan Jhonnes <alanjhonnes@gmail.com>
2021-08-18 18:24:22 -0300

    feat: add user mutation

--- src/mutations/add-user.mutation.ts
+++ src/mutations/add-user.mutation.ts
@@ -1,8 +1,13 @@
+import { users } from "../data";
+import { UserEntity } from "../entities/user.entity";
 import { GQLMutationResolvers } from "../resolvers-types";
 
 export const addUserMutation: GQLMutationResolvers['addUser'] = (obj, args, context) => {
-    return {
-        id: '1',
+    const newUser: UserEntity = {
+        id: users.length.toString(),
         name: args.data.name,
-    }
+    };
+    users.push(newUser);
+
+    return newUser
 }
